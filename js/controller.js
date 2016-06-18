784ba53cfd784ca5845aa837cb8a849a85c1a4ce
menescildir <enes.cildir@hotmail.com>
2016-06-18 19:05:46 +0300

    Second Commit

--- js/controller.js
+++ js/controller.js
@@ -1,1 +1,11 @@
 var app = angular.module('kodofisi.controller',[]);
+
+app.controller("formCtrl", function ($scope) {
+   $scope.form = {
+   	url : ""
+   };
+   $scope.formgoster = function (data) {
+   	console.log(data);
+   	$scope.form.url= ""
+   };
+});
